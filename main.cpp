7d17304f80ffb9ee0045f36de95fbd5f7a140541
Walker Manrique <34077779+walker1239@users.noreply.github.com>
2019-05-09 16:16:59 -0500

    main simplificado

--- main.cpp
+++ main.cpp
@@ -6,47 +6,23 @@
 
 #include "plano.h"
 
-void cachito(plano &p,int tam){
-     if(tam==1)
-          return;
-     p.forward(tam/3);
-     //cachito(p,tam/3);
-     p.right(120);
-     p.forward(tam/3);
-     p.left(60);
-}
 int main(int argc, char **argv)
 {
     
-    plano p(500,500);
+    plano p(700,500);
     p.display(argc,argv);
-
-    /*for(int i=0;i<360;i++){
-     p.forward(2);
-     p.right(1);
-         }*/
-
-    for(int i=0;i<6;i++){
-    cachito(p,100);
-     }
-    //p.move(250,250);
-     /*p.forward(20);
-    p.left(60);
-    p.forward(20);
-    p.right(120);
-    p.forward(20);
-    p.left(60);
-    p.forward(20);
-    p.right(120);
-    p.forward(20);
-    p.left(60);
-    p.forward(20);
-    p.right(120);
-    p.forward(20);
-    p.left(60);
-    p.forward(20);
-    p.right(300);*/
-    //p.forward(100);
+    p.forward(70);
+    p.left(45);
+    p.forward(100);
+    p.penup();
+    p.forward(210);
+    p.left(90);
+    p.pendow();
+    p.forward(50);
+    p.right(30);
+    p.move(250,250);
+    p.forward(100);
+    
     glutMainLoop();
 
 }
